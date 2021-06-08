87096005b9a53484baef7967d37f90fb334ad06e
Kayan Chagas <kayan_edu@hotmail.com>
2021-06-08 20:52:05 -0300

    Ajustes

--- web/scripts.js
+++ web/scripts.js
@@ -1,23 +1,29 @@
 const carregar = async () => {
-  const response = await fetch('http://127.0.0.1:3000/api/fornecedores/');
-  const myJson = await response.json(); //extract JSON from the http response
-  console.log(myJson)
-  // do something with myJson
+    const response = await fetch('http://127.0.0.1:3000/api/fornecedores/');
+    const myJson = await response.json(); //extract JSON from the http response
+    console.log(myJson)
+    // do something with myJson
 }
 
 const cadastrar = async () => {
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-         if (this.readyState == 4 && this.status == 200) {
-             alert(this.responseText);
-         }
+    let $ = document.querySelector.bind(document);//precisa do bind pra manter o document
+    empresa = document.getElementById('empresa').value
+    email = document.getElementById('email').value
+    categoria = document.getElementById('categoria').value
+    valor = document.getElementById('valor').value
+    let fornecedor = {
+        empresa: empresa,
+        email: email,
+        categoria: categoria,
+        valor: valor
+    }
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            alert(this.responseText);
+        }
     };
     await xhttp.open("POST", "http://127.0.0.1:3000/api/fornecedores/", true);
     xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(`{
-    "empresa":"teste",
-    "email":"email@teste.com",
-    "categoria":"testeee",
-    "valor":"20"
-}`);
+    xhttp.send(`${fornecedor}`);
 }
