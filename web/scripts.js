b39c28c105ce9fc72ea6ba909de947076f583a3a
Vinicius Monteiro <dev.viniciusmonteiro@gmail.com>
2021-06-08 21:32:34 -0300

    final

--- web/scripts.js
+++ web/scripts.js
@@ -1,29 +1,42 @@
 const carregar = async () => {
-    const response = await fetch('http://127.0.0.1:3000/api/fornecedores/');
-    const myJson = await response.json(); //extract JSON from the http response
-    console.log(myJson)
-    // do something with myJson
+  const response = await fetch('http://127.0.0.1:3000/api/fornecedores/');
+  const myJson = await response.json(); //extract JSON from the http response
+  console.log(myJson)
+  // do something with myJson
+  let elemento = ''
+
+    myJson.forEach(element => {
+        let aux = `<tr><td>${element.empresa}</td><td>${element.categoria}</td><td>R$ ${element.valor}</td></tr>`
+        elemento += aux
+    });
+    var element = document.getElementById('tabela');
+
+    element.innerHTML = elemento
 }
 
 const cadastrar = async () => {
     var xhttp = new XMLHttpRequest();
     let $ = document.querySelector.bind(document);//precisa do bind pra manter o document
+
     empresa = document.getElementById('empresa').value
     email = document.getElementById('email').value
     categoria = document.getElementById('categoria').value
     valor = document.getElementById('valor').value
-    let fornecedor = {
-        empresa: empresa,
-        email: email,
-        categoria: categoria,
-        valor: valor
-    }
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            alert(this.responseText);
-        }
+
+
+    xhttp.onreadystatechange = function() {
+         if (this.readyState == 4 && this.status == 200) {
+             alert(this.responseText);
+         }
     };
     await xhttp.open("POST", "http://127.0.0.1:3000/api/fornecedores/", true);
     xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(`${fornecedor}`);
+    xhttp.send(`{
+    "empresa":"${empresa}",
+    "email":"${email}",
+    "categoria":"${categoria}",
+    "valor":"${valor}"
+}`);
+
+alert("Produto cadastrado!!!")
 }
